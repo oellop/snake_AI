1d961dc97a0aa7e40f3f190327481e8ed3552f58
Oel <ung.leopold@gmail.com>
2020-03-01 23:15:49 +0100

    Add tail

--- snake.py
+++ snake.py
@@ -5,12 +5,20 @@ from random import randint
 HEIGHT = 25
 WIDTH  = 35
 TIMEOUT = 1000
+BASE_LENGTH = 3
 
 class Snake(object):
     def __init__(self, windows):
         self.head_char = '0'
+        self.tail_char = '='
         self.head_x = 5
         self.head_y = 5
+        self.tail = []
+
+        for i in range(1, BASE_LENGTH):
+            self.tail.append([self.head_y-i, self.head_x])
+        print(self.tail)
+        self.tail_end = self.tail[-1]
         self.score = 0
         self.window = window
         self.direction = KEY_RIGHT
@@ -23,9 +31,14 @@ class Snake(object):
 
     def render(self):
         self.window.addstr(self.head_x, self.head_y, self.head_char)
+        self.window.addstr(0, 20, '{}'.format(self.tail_end))
+        for (x,y) in self.tail:
+            self.window.addstr(y, x, self.tail_char)
 
     def eat_food(self):
         self.score+=1
+        self.tail.append(self.tail_end)
+        print(self.tail)
 
     def move_up(self):
         self.head_x -= 1
@@ -51,8 +64,14 @@ class Snake(object):
         self.direction = dir
 
     def update(self):
-        self.direction_map[self.direction]()
+        last_coord = [self.head_y, self.head_x]
 
+
+        self.direction_map[self.direction]()
+        for i in range(len(self.tail)):
+            (last_coord[0], last_coord[1]), (self.tail[i][0], self.tail[i][1]) = (self.tail[i][0], self.tail[i][1]), (last_coord[0], last_coord[1])
+
+        self.tail_end = [last_coord[0], last_coord[1]]
 
 class Food(object):
     def __init__(self, window, char='Q'):
@@ -74,8 +93,6 @@ window.keypad(1)
 curses.noecho()
 curses.curs_set(0)
 window.border(0)
-i = 1
-event = 0
 exit = 0
 snake = Snake(window)
 food = Food(window)
